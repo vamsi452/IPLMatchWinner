64699daea1973513f1c3bf306ba71b0f817af57d
Vamsi Kallepalli <vkallepalli@uworld.com>
2017-04-10 14:36:20 -0500

    first commit

---
+++ MatchDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MatchDetailsViewController.h
+//  IPLMatchWinner
+//
+//  Created by Vamsi Kallepalli on 4/8/17.
+//  Copyright © 2017 Vamsi Kallepalli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MatchDetailsViewController : UIViewController
+
+@end
---
+++ MatchDetailsViewController.m
@@ -0,0 +1,37 @@
+//
+//  MatchDetailsViewController.m
+//  IPLMatchWinner
+//
+//  Created by Vamsi Kallepalli on 4/8/17.
+//  Copyright © 2017 Vamsi Kallepalli. All rights reserved.
+//
+
+#import "MatchDetailsViewController.h"
+
+@interface MatchDetailsViewController ()
+
+@end
+
+@implementation MatchDetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
